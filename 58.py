3bfaee1d6f9bd3383a8b5b5eb00c55621f92d756
Marcos Boscariol <118377554+marcosboscariol@users.noreply.github.com>
2024-08-16 21:25:38 -0300

    delete of the previous file

--- 58.py
+++
@@ -1,9 +0,0 @@
-# Calcular a área de uma circunferência
-
-import math
-
-raio = float(input("Informe o raio da circunferência: "))
-
-area = math.pi * raio ** 2
-
-print(f"A área de uma circunferência de {raio} é {round(area,2)}")
