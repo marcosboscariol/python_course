177b6da81c0b9195b21235e26907a8e3a78c8e84
Marcos Boscariol <118377554+marcosboscariol@users.noreply.github.com>
2024-08-16 22:47:44 -0300

    improving def calcular_area_circunferencia

--- 58_area_circunferencia.py
+++ 58_area_circunferencia.py
@@ -5,11 +5,10 @@ import math
 
 def calcular_area_circunferencia(raio):
 
-    area = math.pi * raio ** 2
-
-    print(f"A área de uma circunferência de {raio} é {round(area,2)}")
+    return math.pi * raio ** 2
 
 
 raio_usuario = float(input("Informe o raio: "))
 
-calcular_area_circunferencia(raio_usuario)
+print(
+    f"A área de uma circunferência de {raio_usuario} é {round(calcular_area_circunferencia(raio_usuario),2)}")
