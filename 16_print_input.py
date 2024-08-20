2e89f860778cdfeb7f333d8b519c901778bbd7b4
Marcos Boscariol <118377554+marcosboscariol@users.noreply.github.com>
2024-08-20 13:39:03 -0300

    add file

---
+++ 16_print_input.py
@@ -0,0 +1,21 @@
+# print() -> Mostrar algo na tela
+
+print('Olá Mundo')
+
+# Vaiáveis são espaços de memória utilziados para armazenar uma determinada informação
+
+x = 5
+
+y = 6
+
+soma = x+y
+
+print(f'O resultado da soma de {x} e {y} é {soma}')
+
+# input() -> Recebe alguma informação do usuário e armazena em um espaço de memória
+
+nome = input('Qual seu nome? ')
+
+idade = int(input('Qual sua idade? '))
+
+print(f'{nome} tem {idade} anos.')
