5037183e829cfb20b12d34eafc810628380bb800
Marcos Boscariol <118377554+marcosboscariol@users.noreply.github.com>
2024-08-20 19:52:06 -0300

    add file and add folder trilha_1_fundamentos

---
+++ trilha_1_fundamentos/18_indicadores_e_palavras_reservaads.py
@@ -0,0 +1,4 @@
+# Identificadores -> Nome de variáveis, funções, calsses e módulos
+
+# Identificadores não podem iniciar com um dígito, não podem conter palavras chaves, não
+# podem serem utilizadasespaóes e caracteres especiais
