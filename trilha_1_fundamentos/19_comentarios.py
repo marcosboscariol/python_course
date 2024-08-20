9d002f83ba26f62eefb58a1b1d78d46ee53598c4
Marcos Boscariol <118377554+marcosboscariol@users.noreply.github.com>
2024-08-20 19:54:46 -0300

    add file

---
+++ trilha_1_fundamentos/19_comentarios.py
@@ -0,0 +1,7 @@
+# Comentários são linhas que não serão analisadas pelo interpretador
+
+# Utilizados para documentar os códigos
+
+# Comentários de uma única linha -> #
+
+# Comentários de várias linhas -> """ """
