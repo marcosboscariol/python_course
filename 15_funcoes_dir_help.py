d89895d0b9101fff0f6ae78790a6fae5cd21219a
Marcos Boscariol <118377554+marcosboscariol@users.noreply.github.com>
2024-08-20 13:09:37 -0300

    add file

---
+++ 15_funcoes_dir_help.py
@@ -0,0 +1,9 @@
+# dir(<parametro>) -> Lista todos os membros de um objeto ou escopo específico
+
+print(dir(8))
+
+print(dir(__builtins__))
+
+# help(<parâmetro>) -> Ajuda apra algum objeto, método, tipo de dado ou contexto específico
+
+print(help(print))
