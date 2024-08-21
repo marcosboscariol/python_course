0a7733f5967e9216de5d1bf342a46269903faa66
Marcos Boscariol <118377554+marcosboscariol@users.noreply.github.com>
2024-08-21 13:33:40 -0300

    add file

---
+++ trilha_1_fundamentos/023_int.py
@@ -0,0 +1,18 @@
+print(type(10))
+
+num = 10
+
+print(type(num))
+
+# Divisão de int retornom float
+
+print(type(10/2))
+
+
+# Não há limites de espaço, pois eles são armazenados em objetos, dinâmicamente alocado
+
+print(2**2000)
+
+# É possível separar os milhares por um _
+
+print(1_000_000)
