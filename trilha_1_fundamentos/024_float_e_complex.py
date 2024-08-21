c310efa3965d198cfeac7479001e12a90482e279
Marcos Boscariol <118377554+marcosboscariol@users.noreply.github.com>
2024-08-21 13:45:51 -0300

    add file

---
+++ trilha_1_fundamentos/024_float_e_complex.py
@@ -0,0 +1,29 @@
+# float são os números reais (decimais)
+
+# complex são os números complexos
+
+print(type(10.0))
+
+num = 10.0
+
+print(type(num))
+
+print(type(10.44+1.56))
+
+inteiro = 5
+
+num = float(inteiro)
+
+print(num)
+
+# float tem uma limitação e relação ao armazenamento
+
+# print(2.45 ** 1000)
+
+# É possível separar os milhares com um _
+
+print(1_000_000.000_000_1)
+
+complexo = 5j
+
+print(type(complexo))
