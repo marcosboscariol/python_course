48a45e280c71e20c81a7c6125345d607b662383d
Marcos Boscariol <118377554+marcosboscariol@users.noreply.github.com>
2024-08-21 13:20:59 -0300

    add file

---
+++ trilha_1_fundamentos/021_variavel.py
@@ -0,0 +1,5 @@
+"""
+Variável é espaço de memória do computador destinado a um dado que pode ser alterado durante a execução de um programa
+O espaço de memória tem um endereço
+Existe um rótulo (nome da variável) que vai associar esse nome a tal endereço de memória
+"""
