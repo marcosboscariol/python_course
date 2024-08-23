772d86312b1cfd44439c468a7720fd5bd04ccbdf
Marcos Boscariol <118377554+marcosboscariol@users.noreply.github.com>
2024-08-22 22:52:03 -0300

    add file

---
+++ trilha_1_fundamentos/029_linearizacao_de_expressoes.py
@@ -0,0 +1,11 @@
+# Em programação não é possível realizar expressões matemáticas da forma convencional
+
+# É nevessário realizar uma linearização
+
+'''
+x =    3y    + 2y
+    --------
+    5y + 7
+
+x = ((3*y)/(5*y + 7))+(2y)
+'''
