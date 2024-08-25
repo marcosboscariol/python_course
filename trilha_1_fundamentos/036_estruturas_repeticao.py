531cdd2b8b028b21e678293f9123e3936045b0ed
Marcos Boscariol <118377554+marcosboscariol@users.noreply.github.com>
2024-08-24 22:19:55 -0300

    add file

---
+++ trilha_1_fundamentos/036_estruturas_repeticao.py
@@ -0,0 +1,3 @@
+# Permitem que uma determinada ação seja sexecutada mais de uma vez sem que tenhamos que executar novamente todo o script.
+
+# O número de reppetições pode ser determinado ou indeterminado
