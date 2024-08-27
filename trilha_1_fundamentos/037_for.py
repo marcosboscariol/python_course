3d2ced422ecad6a62739f56fa990f7c7e4867baa
Marcos Boscariol <118377554+marcosboscariol@users.noreply.github.com>
2024-08-26 22:32:33 -0300

    add fatorial measure

--- trilha_1_fundamentos/037_for.py
+++ trilha_1_fundamentos/037_for.py
@@ -6,3 +6,11 @@ for <variável de iteração> in <sequencia>:
 '''
 for i in range(1, 6):
     print(i)
+
+fatorial_usuario = int(input('Informe um número positivo maior que 0: '))
+
+fatorial = 1
+for i in range(1, fatorial_usuario + 1):
+    fatorial = fatorial * i
+
+print(f'O fatorial de {fatorial_usuario} é {fatorial}')
