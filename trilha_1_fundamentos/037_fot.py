8cd7d0294f54e527b85e521047f982143b512697
Marcos Boscariol <118377554+marcosboscariol@users.noreply.github.com>
2024-08-26 22:21:14 -0300

    add file

---
+++ trilha_1_fundamentos/037_fot.py
@@ -0,0 +1,10 @@
+# Repetição definida, sabemos a quantidade de vezes que o a ação será repetida
+
+'''
+for <variável de iteração> in <sequencia>:
+    comando
+'''
+soma = 0
+for i in range(1, 6):
+    soma += 1
+    print(soma)
