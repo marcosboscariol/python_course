f6d3328c6e524bd15240a3f795a0bb2f04f85cce
Marcos Boscariol <118377554+marcosboscariol@users.noreply.github.com>
2024-09-01 23:01:14 -0300

    add file

---
+++ trilha_1_fundamentos/042_listas.py
@@ -0,0 +1,19 @@
+'''
+ São coleções de dados ordenados, separados por vírgula e que estão dentro de colchetes
+
+Listas Homogêneas:
+['a', 'b', 'c']
+[1, 2, 3]
+['texto1', 'texto2', 'texto3']
+
+Listas Heterogêneas:
+['a', 1, 'texto1', ['b', 2, 'texto2']]
+'''
+
+lista_de_numeros = [6, 3, 7, 9]
+
+print(type(lista_de_numeros))
+
+lista_heterogenea = ['texto', 33, 45.98, False, [1, 2, 3]]
+
+lista_vazia = []
